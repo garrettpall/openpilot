fd18e73ead3b7f51fc31207deea6ef1c8f6dfcd5
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-11-11 09:38:16 -0500

    New Tune

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -136,16 +136,17 @@ class CarInterface(CarInterfaceBase):
 
       # Tuning for experimental long
       if use_new_api:
-        ret.longitudinalTuning.kiV = [2.0, 1.5]
+        ret.longitudinalTuning.kiV = [0.5, 0.5]
         ret.vEgoStopping = 0.1
         ret.vEgoStarting = 0.1
       else:
         ret.longitudinalTuning.kpV = [2.0, 1.5]
         ret.longitudinalTuning.kiV = [0.72]
 
-      ret.stoppingDecelRate = 2.0  # reach brake quickly after enabling
+      ret.stoppingDecelRate = 1.0  # reach brake quickly after enabling
       ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
+      ret.stopAccel = -0.25
 
       if experimental_long and not cslcEnabled:
         ret.pcmCruise = False
@@ -175,7 +176,7 @@ class CarInterface(CarInterfaceBase):
 
       # Tuning
       if use_new_api:
-        ret.longitudinalTuning.kiV = [2.4, 1.5]
+        ret.longitudinalTuning.kiV = [0.5, 0.5]
       else:
         ret.longitudinalTuning.kpV = [2.4, 1.5]
         ret.longitudinalTuning.kiV = [0.36]
